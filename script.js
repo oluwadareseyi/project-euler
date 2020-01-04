685d92a64ae13d60135bad185195cad20e0a8df3
oluwadareseyi <oluwadareseyii@gmail.com>
2020-01-04 09:10:16 +0100

    finished day 3

--- script.js
+++ script.js
@@ -11,13 +11,26 @@
 
 //   console.log(multiplesOf3and5(1000));
 
-const fiboEvenSum = n =>
-  [...Array(n)]
-    .reduce(
-      (arr, _, i) => arr.concat(i > 1 ? arr[i - 1] + arr[i - 2] : i + 1),
-      []
-    )
-    .filter(a => a % 2 === 0)
-    .reduce((a, b) => a + b);
+// const fiboEvenSum = n =>
+//   [...Array(n)]
+//     .reduce(
+//       (arr, _, i) => arr.concat(i > 1 ? arr[i - 1] + arr[i - 2] : i + 1),
+//       []
+//     )
+//     .filter(a => a % 2 === 0)
+//     .reduce((a, b) => a + b);
 
-console.log(fiboEvenSum(10));
+// console.log(fiboEvenSum(10));
+
+const largestPrimeFactor = (number) => {
+    let arr = [];
+    for (let i = 2; i <= number; i++) {
+        while ((number % i) === 0) {
+            arr.push(i);
+            number /= i;
+        }
+    }
+    return arr.sort((a, b) => b - a)[0];
+  }
+
+console.log(largestPrimeFactor(13195));
