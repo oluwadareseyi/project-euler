f577390ffb9b7bf50a75c83c7dd35cb16fd01b78
oluwadareseyi <oluwadareseyii@gmail.com>
2020-01-04 08:25:23 +0100

    finished day 2

--- script.js
+++ script.js
@@ -7,14 +7,17 @@
 
 //     return arrs.filter(arr => arr % 3 === 0 || arr % 5 === 0).reduce((a, b) => a + b)
 
-    
-    
 // }
-  
-  
+
 //   console.log(multiplesOf3and5(1000));
 
-  
+const fiboEvenSum = n =>
+  [...Array(n)]
+    .reduce(
+      (arr, _, i) => arr.concat(i > 1 ? arr[i - 1] + arr[i - 2] : i + 1),
+      []
+    )
+    .filter(a => a % 2 === 0)
+    .reduce((a, b) => a + b);
 
-
-  
+console.log(fiboEvenSum(10));
